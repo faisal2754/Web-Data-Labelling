94687342dcd96b90498c48d8744f29c72e538acb
bigtrist <2090454@students.wits.ac.za>
2021-04-15 10:20:24 +0200

    Testing heroku.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+//const mongoose = require("mongoose");
+//const dotenv = require("dotenv");
 
-dotenv.config();
+//dotenv.config();
 
 // Import routes
-const authRoute = require("../routes/auth");
+//const authRoute = require("../routes/auth");
 
 // Connect to DB
-try {
+/*try {
   mongoose.connect(
     process.env.DB_CONNECT,
     { useUnifiedTopology: true, useNewUrlParser: true },
@@ -24,5 +24,5 @@ app.use(express.json());
 
 // Route middleware?
 app.use("/api/user", authRoute);
-
+*/
 app.listen(3000, () => console.log("Server running"));
