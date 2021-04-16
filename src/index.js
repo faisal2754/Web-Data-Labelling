d364d8710a53ac0df8379cb41614f6b370a665e3
bigtrist <2090454@students.wits.ac.za>
2021-04-16 16:56:17 +0200

    l

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });*/
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/src/index.html"));
+  res.sendFile(path.join(__dirname + "/../index.html"));
 });
 
 const port = process.env.PORT || "5000";
