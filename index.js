e035541ac784dcb73842d9ebe3d4734ff9bddcea
bigtrist <2090454@students.wits.ac.za>
2021-04-21 16:27:23 +0200

    Added code for sessions.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const { default: axios } = require("axios");
+const authRoute = require("./routes/auth")
+const flash = require('express-flash')
+const session = require('express-session');
+const passport = require("passport");
+// if(process.env.NODE_ENV !== 'production'){
+//   require('dotenv').config()
+// }
 
 const app = express();
 var x = path.join(__dirname, "public")
@@ -9,7 +16,33 @@ const port = process.env.PORT || 3000
 
 app.set("view engine","ejs");
 app.use(express.static(x));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false}))
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use("", authRoute)
+app.use(flash())
+app.use(session({
+    secret: 'bruh',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session()  )
+
+app.get("/", (req, res) => {
+    res.render("index"); // index refers to index.ejs
+});
+
+app.listen(port, () => {
+  console.log("server started on port 3000");
+});
+
+
+
+
+
+
+
+
 
 // app.post("/login", (req, res) =>{
 //     const {name, password} = req.body;
@@ -19,52 +52,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //             username: name,
 //         });
 //     } else {
-//         res.render("failure");
+//         res.render("register");
 //     }
 // })
 
-//Following section contians all the routes to the different pages and renders them
+// //Following section contians all the routes to the different pages and renders them
 
-app.get("/login", (req,res) =>{
-  res.render("login");
-})
+// app.get("/login", (req,res) =>{
+//   res.render("login");
+// })
 
-app.get("/register", (req,res) =>{
-  res.render("register");
-})
+// app.get("/register", (req,res) =>{
+//   res.render("register");
+// })
 
-app.get("/contact-us", (req,res) =>{
-  res.render("contact-us");
-});
+// app.get("/contact-us", (req,res) =>{
+//   res.render("contact-us");
+// });
 
-app.get("/about-us", (req,res) => {
-  res.render("about-us");
-})
+// app.get("/about-us", (req,res) => {
+//   res.render("about-us");
+// })
 
-app.get("/terms-conditions", (req,res) =>{
-  res.render("terms-conditions");
-})
+// app.get("/terms-conditions", (req,res) =>{
+//   res.render("terms-conditions");
+// })
 
-app.get("/category",(req,res) =>{
-  res.render("category");
-})
+// app.get("/category",(req,res) =>{
+//   res.render("category");
+// })
 
-app.get("/ad-listing", (req,res) =>{
-  res.render("ad-listing");
-})
+// app.get("/ad-listing", (req,res) =>{
+//   res.render("ad-listing");
+// })
 
-app.get("/dashboard", (req,res) =>{
-  res.render("dashboard");
-})
+// app.get("/dashboard", (req,res) =>{
+//   res.render("dashboard");
+// })
 
-app.get("/user-profile", (req,res) =>{
-  res.render("user-profile")
-})
+// app.get("/user-profile", (req,res) =>{
+//   res.render("user-profile")
+// })
 
-app.get("/", (req, res) => {
-    res.render("index"); // index refers to index.ejs
-});
+// app.get("/", (req, res) => {
+//     res.render("index"); // index refers to index.ejs
+// });
 
-app.listen(port, () => {
-  console.log("server started on port 3000");
-});
