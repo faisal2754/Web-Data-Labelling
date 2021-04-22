ed838e38681a70c9268b7295c3fbe30a23fdfc37
Faisal <2095208@students.wits.ac.za>
2021-04-23 01:46:23 +0200

    Configured session management

--- index.js
+++ index.js
@@ -1,103 +1,61 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const { default: axios } = require("axios");
-const authRoute = require("./routes/auth")
-const flash = require('express-flash')
-const session = require('express-session');
-const passport = require("passport");
-// if(process.env.NODE_ENV !== 'production'){
-//   require('dotenv').config()
-// }
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
-const app = express();
-var x = path.join(__dirname, "public")
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const { default: axios } = require('axios')
+const authRoute = require('./routes/auth')
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+const app = express()
+const mongoose = require('mongoose')
+
+var x = path.join(__dirname, 'public')
 const port = process.env.PORT || 3000
 
-app.set("view engine", "ejs");
-app.use(express.static(x));
-app.use(express.urlencoded({ extended: false}))
+app.set('view engine', 'ejs')
+app.use(express.static(x))
+app.use(express.urlencoded({ extended: false }))
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use("", authRoute)
-app.use(flash())
-app.use(session({
-    secret: 'bruh',
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session()  )
 
-app.get("/", (req, res) => {
-    res.render("index"); // index refers to index.ejs
-});
+//connect to mongodb
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
+            console.log('connected to db!')
+        )
+    } catch (error) {
+        console.log(error)
+    }
+
+    //safetly remove later
+    mongoose.connection.on('error', (err) => {
+        console.log(err)
+    })
+    //end of safety
+}
+connectDB()
+
+//middleware
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
+    })
+)
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+//routes
+
+app.use('', authRoute)
 
 app.listen(port, () => {
-  console.log("server started on port 3000");
-});
-
-
-
-
-
-
-
-
-
-// app.post("/login", (req, res) =>{
-//     const {name, password} = req.body;
-
-//     if (name === "admin" && password === "admin"){
-//         res.render("success", {
-//             username: name,
-//         });
-//     } else {
-//         res.render("register");
-//     }
-// })
-
-// //Following section contians all the routes to the different pages and renders them
-
-app.get("/login", (req, res) => {
-  res.render("login");
+    console.log('server started on port 3000')
 })
-
-app.get("/register", (req, res) => {
-  res.render("register");
-})
-
-app.get("/contact-us", (req, res) => {
-  res.render("contact-us");
-});
-
-app.get("/about-us", (req, res) => {
-  res.render("about-us");
-})
-
-app.get("/terms-conditions", (req, res) => {
-  res.render("terms-conditions");
-})
-
-app.get("/availableJobs", (req, res) => {
-  res.render("availableJobs");
-})
-
-app.get("/ad-listing", (req, res) => {
-  res.render("ad-listing");
-})
-
-app.get("/dashboard", (req, res) => {
-  res.render("dashboard");
-})
-app.get("/howtopage", (req, res) => {
-  res.render("howtopage");
-})
-
-app.get("/user-profile", (req, res) => {
-  res.render("user-profile")
-})
-
-app.get("/", (req, res) => {
-  res.render("index"); // index refers to index.ejs
-});
-
--- passport-config.js
+++ passport-config.js
@@ -1,27 +1,29 @@
 const LocalStrategy = require('passport-local').Strategy
 
-function initialize(passport, getUserByEmail){
-    
-    const authenticateUser = (email, password, done) => {
-        const user = getUserByEmail(email)
-         if(user == null){
-            return done(null, false, {message: 'No user with that email'})
-         }
+function initialize(passport, getUserByEmail) {
+    const authenticateUser = async (email, password, done) => {
+        const user = await getUserByEmail(email)
 
-         try{
-            if(password === user.password){
+        if (user == null) {
+            return done(null, false, { message: 'No user with that email' })
+        }
+
+        try {
+            if (password === user.password) {
                 return done(null, user)
-            }else{
-                return done(null, false, {message: 'Password incorrect'})
+            } else {
+                return done(null, false, { message: 'Password incorrect' })
             }
-         }catch (e){
+        } catch (e) {
             return done(e)
-         }
+        }
     }
 
-    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
-    passport.serializeUser((user, done) => {})
-    passport.deserializeUser((id, done) => {})
+    passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.email))
+    passport.deserializeUser((email, done) => {
+        return done(null, getUserByEmail(email))
+    })
 }
 
 module.exports = initialize
