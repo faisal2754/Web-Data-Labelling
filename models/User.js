fe60fbd0a91eaced8d4a6f1b9d6a85bc0f30c7b6
Faisal <2095208@students.wits.ac.za>
2021-05-03 13:49:01 +0200

    Added timestamps to User model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        require: true,
-        minLength: 1,
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            require: true,
+            minLength: 1
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            minLength: 1
+        },
+        password: {
+            type: String,
+            require: true,
+            minLength: 1
+        }
     },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-        minLength: 1,
-    },
-    password: {
-        type: String,
-        require: true,
-        minLength: 1,
-    },
-})
+    {
+        timestamps: true
+    }
+)
 
 module.exports = mongoose.model('User', userSchema)
