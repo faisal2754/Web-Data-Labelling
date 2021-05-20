58117bdefc8570c1869bcd8f204d8fb167036448
Sipho SK <sipssman@gmail.com>
2021-05-20 09:18:40 +0200

    User now redirects to login if they're not logged in

--- routes/auth.js
+++ routes/auth.js
@@ -11,126 +11,133 @@ const googleService = require('../googleServices')
 const service = new googleService()
 
 initialize(passport, async (email) => {
-    // returning user with logged in email
-    const user = await User.findOne({ email: email })
-    return user
+  // returning user with logged in email
+  const user = await User.findOne({ email: email })
+  return user
 })
 
 router.post('/register', async (req, res) => {
-    const name = req.body.name
-    const email = req.body.email
-    const password = req.body.password
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
 
-    const user = new User({
-        name: name,
-        email: email,
-        password: password,
-    })
+  const user = new User({
+    name: name,
+    email: email,
+    password: password,
+  })
 
-    try {
-        const error = user.validateSync()
-        if (error) {
-            throw new Error('Invalid user')
-        }
-
-        const savedUser = await user.save()
-        res.render('login', { status: true })
-    } catch (e) {
-        // console.log(e)
-        res.render('register', { error: true })
+  try {
+    const error = user.validateSync()
+    if (error) {
+      throw new Error('Invalid user')
     }
+
+    const savedUser = await user.save()
+    res.render('login', { status: true })
+  } catch (e) {
+    // console.log(e)
+    res.render('register', { error: true })
+  }
 })
 
 router.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: 'dashboard',
-        failureRedirect: 'login',
-        failureFlash: true,
-    })
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: 'dashboard',
+    failureRedirect: 'login',
+    failureFlash: true,
+  })
 )
 
 router.post(
-    '/create-job',
-    checkAuthenticated,
-    localStorage.array('image'),
-    async (req, res) => {
-        const title = req.body.title
-        const description = req.body.description
-        const labels = req.body.labels
-        const credits = req.body.credits
-        const emailOwner = await req.user
+  '/create-job',
+  checkAuthenticated,
+  localStorage.array('image'),
+  async (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+    const labels = req.body.labels
+    const credits = req.body.credits
+    const emailOwner = await req.user
 
-        const job = await new Job({
-            title: title,
-            description: description,
-            credits: credits,
-            labels: labels,
-            emailOwner: emailOwner.email,
+    const job = await new Job({
+      title: title,
+      description: description,
+      credits: credits,
+      labels: labels,
+      emailOwner: emailOwner.email,
+    })
+
+    try {
+      const savedJob = await job.save()
+      const imgPath = 'public/uploads/'
+      const localImgArr = fs.readdirSync(imgPath)
+      const driveImgArr = []
+      service
+        .uploadFiles(localImgArr, imgPath)
+        .then((results) => {
+          results.forEach((result) => {
+            driveImgArr.push(`https://drive.google.com/uc?id=${result.data.id}`)
+            fs.rm(imgPath + result.data.name, (err) => {
+              if (err) {
+                console.log(err)
+              }
+            })
+          })
+          Job.findOneAndUpdate(
+            { _id: savedJob._id },
+            { $set: { images: driveImgArr } },
+            (err, ans) => {
+              if (err) {
+                console.log(err)
+              }
+            }
+          )
+          console.log('Done uploading all images')
+          res.redirect('/dashboard')
         })
-
-        try {
-            const savedJob = await job.save()
-            const imgPath = 'public/uploads/'
-            const localImgArr = fs.readdirSync(imgPath)
-            const driveImgArr = []
-            service
-                .uploadFiles(localImgArr, imgPath)
-                .then((results) => {
-                    results.forEach((result) => {
-                        driveImgArr.push(
-                            `https://drive.google.com/uc?id=${result.data.id}`
-                        )
-                        fs.rm(imgPath + result.data.name, (err) => {
-                            if (err) {
-                                console.log(err)
-                            }
-                        })
-                    })
-                    Job.findOneAndUpdate(
-                        { _id: savedJob._id },
-                        { $set: { images: driveImgArr } },
-                        (err, ans) => {
-                            if (err) {
-                                console.log(err)
-                            }
-                        }
-                    )
-                    console.log('Done uploading all images')
-                    res.redirect('/dashboard')
-                })
-                .catch((e) => {
-                    console.log(e)
-                })
-        } catch (e) {
-            console.log(e)
-            res.redirect('/')
-        }
+        .catch((e) => {
+          console.log(e)
+        })
+    } catch (e) {
+      console.log(e)
+      res.redirect('/')
     }
+  }
 )
 
 router.post('/acceptJob', checkAuthenticated, async (req, res) => {
+  if (req.isAuthenticated()) {
     const user = await req.user
     const userEmail = user.email
     const jobId = req.body.jobId
     const result = await Job.findByIdAndUpdate(
-        //Identifies which jobs we are adding to
-        jobId,
-        {
-            // Add to set insures that a user doesn't get added to a job multiple times
-            $addToSet: {
-                emailLabellers: userEmail,
-            },
-        }
+      //Identifies which jobs we are adding to
+      jobId,
+      {
+        // Add to set insures that a user doesn't get added to a job multiple times
+        $addToSet: {
+          emailLabellers: userEmail,
+        },
+      }
     )
-    // console.log(result)
-    // console.log('Added to the Job')
+
     res.redirect('/dashboard')
+  }
+  else{
+    res.redirect('/login')
+  }
+
+  // console.log(result)
+  // console.log('Added to the Job')
+
+  
 })
 
 router.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/')
+  req.logOut()
+  res.redirect('/')
 })
 
 module.exports = router
