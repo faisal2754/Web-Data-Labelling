10e2b941078eb5bc465c6163c4353cef479f9cbb
jononunes <jnunes038@gmail.com>
2021-06-07 12:25:16 +0200

    DB error

--- routes/protected.js
+++ routes/protected.js
@@ -169,7 +169,7 @@ router.post('/job-label-update', checkAuthenticated, async (req, res) => {
 router.get('/do-job/:id', async (req, res) => {
     const user = await req.user
 
-    const labellingData = await Labelling.findOne({
+    const labellingData = Labelling.findOne({
         jobId: req.params.id
     })
 
